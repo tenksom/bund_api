a2b0d5b9d6d744ff84cc7acb7cec244cd9a2223c
tenksom <joachim@stramke.com>
2023-12-30 16:06:22 +0100

    edit names

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import requests
-from autobahn import Autobahn
-from wasserpegel import Wasserpegel
+from wasserpegelapi import WasserpegelAPI
+
 if __name__ == "__main__":
-    autobahn = Autobahn()
-    # for highway in autobahn.get_highway():
-    #     print(highway + ": ")
-    #     print(autobahn.get_available_cameras(highway))
-
-    wasserpegel = Wasserpegel()
-    print(f"Wasserpegel: {wasserpegel.currentWasserpegelBautzen()}")
-
-
+    wasserpegel = WasserpegelAPI()
+    bautzen = wasserpegel.currentWasserpegelBautzen()
+    print(f"Wasserpegel in Bautzen um {bautzen.time}: {bautzen.pegel}")
+    schirgiswalde = wasserpegel.currentWasserpegelSpreeSchirgiswalde()
+    print(f"Wasserpegel in Schirgiswalde um {schirgiswalde.time}: {schirgiswalde.pegel}")
+    dresden = wasserpegel.currentWasserpegelDresden()
+    print(f"Wasserpegel in Dresden um {dresden.time}: {dresden.pegel}")
--- wasserpegelapi.py
+++ wasserpegelapi.py
@@ -2,18 +2,28 @@ import requests
 import pandas as pd
 
 
-class Wasserpegel:
+class WasserpegelAPI:
     def __init__(self):
         self.schirgiswalde = 'https://www.umwelt.sachsen.de/umwelt/infosysteme/hwims/portal/web/wasserstand-pegel-582010'
         self.bautzen = 'https://www.umwelt.sachsen.de/umwelt/infosysteme/hwims/portal/web/wasserstand-pegel-10000100'
+        self.dresden = ('https://www.umwelt.sachsen.de/umwelt/infosysteme/hwims/portal/web/wasserstand-pegel-501060')
 
     def getWasserpegel(self, url):
         html = requests.get(url).content
         df_list = pd.read_html(html)
-        return int(df_list[0].iloc[0]['W'])
+
+        return Wasserpegel(df_list[0].iloc[0])
 
     def currentWasserpegelSpreeSchirgiswalde(self) -> int:
         return self.getWasserpegel(self.schirgiswalde)
 
     def currentWasserpegelBautzen(self) -> int:
         return self.getWasserpegel(self.bautzen)
+
+    def currentWasserpegelDresden(self) -> int:
+        return self.getWasserpegel(self.dresden)
+
+class Wasserpegel:
+    def __init__(self, dataframe):
+        self.time = dataframe['Zeitpunkt']
+        self.pegel = dataframe['W']
