352e51f25dbb84e76f0fc44e4a66603b8b5009a5
tenksom <joachim@stramke.com>
2023-12-30 19:12:43 +0100

    added print functions and equals

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from wasserpegel.wasserpegelapi import Wasserpegelapi
-
+from time import sleep
 if __name__ == "__main__":
     wasserpegel = Wasserpegelapi()
+    print("Getting data...")
     bautzen = wasserpegel.current_wasserpegel_bautzen()
-    print(f"Wasserpegel in Bautzen um {bautzen.time}: {bautzen.pegel}")
-
     schirgiswalde = wasserpegel.current_wasserpegel_schirgiswalde()
-    print(f"Wasserpegel in Schirgiswalde um {schirgiswalde.time}: {schirgiswalde.pegel}")
-
     dresden = wasserpegel.current_wasserpegel_dresden()
+    print(f"Wasserpegel in Bautzen um {bautzen.time}: {bautzen.pegel}")
+    print(f"Wasserpegel in Schirgiswalde um {schirgiswalde.time}: {schirgiswalde.pegel}")
     print(f"Wasserpegel in Dresden um {dresden.time}: {dresden.pegel}")
+    print("-" * 40)
+    booleans = [False, False, False]
+    while True:
+        bautzen_updated = wasserpegel.current_wasserpegel_bautzen()
+        if bautzen != bautzen_updated:
+            bautzen = bautzen_updated
+            print(bautzen)
+            booleans[0] = True
+        schirgiswalde_updated = wasserpegel.current_wasserpegel_schirgiswalde()
+        if schirgiswalde != schirgiswalde_updated:
+            schirgiswalde = schirgiswalde_updated
+            print(schirgiswalde)
+            booleans[1] = True
+        dresden_updated = wasserpegel.current_wasserpegel_dresden()
+        if dresden != dresden_updated:
+            dresden = dresden_updated
+            print(dresden)
+            booleans[2] = True
+        if (booleans[0] and booleans[1] and booleans[2]):
+            print("-" * 40)
+            booleans = [False, False, False]
+        sleep(60)
--- wasserpegel/wasserpegel.py
+++ wasserpegel/wasserpegel.py
@@ -1,5 +1,13 @@
-
 class Wasserpegel:
-    def __init__(self, dataframe):
+    def __init__(self, name, dataframe):
+        self.name = name
         self.time = dataframe['Zeitpunkt']
         self.pegel = dataframe['W']
+
+    def __eq__(self, other):
+        if other.time == self.time and other.pegel == self.pegel:
+            return True
+        return False
+
+    def __str__(self):
+        return f"Wasserpegel in {self.name} um {self.time}: {self.pegel}"
