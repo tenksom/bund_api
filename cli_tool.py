3c82f3bcedc203572e8a5dcd5d8bc6aa4ca8387e
tenksom <joachim@stramke.com>
2024-01-23 20:55:03 +0100

    cleanup and moving cli output to a new file

---
+++ cli_tool.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+from wasserpegel.sachsen import wasserpegel
+
+
+def cli_tool():
+    print("Getting data...")
+    places = ["bautzen", "schirgiswalde", "dresden"]
+    bautzen = wasserpegel.get_wasserpegel(places[0])
+    schirgiswalde = wasserpegel.get_wasserpegel(places[1])
+    dresden = wasserpegel.get_wasserpegel(places[2])
+    print(bautzen)
+    print(schirgiswalde)
+    print(dresden)
+    print("-" * 40)
+    booleans = [False, False, False]
+    while True:
+        bautzen_updated = wasserpegel.get_wasserpegel(places[0])
+        if bautzen != bautzen_updated:
+            bautzen = bautzen_updated
+            print(bautzen)
+            booleans[0] = True
+        schirgiswalde_updated = wasserpegel.get_wasserpegel(places[1])
+        if schirgiswalde != schirgiswalde_updated:
+            schirgiswalde = schirgiswalde_updated
+            print(schirgiswalde)
+            booleans[1] = True
+        dresden_updated = wasserpegel.get_wasserpegel(places[2])
+        if dresden != dresden_updated:
+            dresden = dresden_updated
+            print(dresden)
+            booleans[2] = True
+        if booleans[0] and booleans[1] and booleans[2]:
+            print("-" * 40)
+            booleans = [False, False, False]
+        sleep(60)
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from starlette.responses import JSONResponse
-
 from wasserpegel.sachsen.wasserpegelsachsen import WasserpegelSachsen
-from time import sleep
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import RedirectResponse
-import starlette.status as status
+from cli_tool import cli_tool
 
-app = FastAPI(redoc_url=None)
+app = FastAPI(redoc_url=None, docs_url="/")
 wasserpegel = WasserpegelSachsen()
-
-
-
-
-@app.get("/")
-async def redirect_to_docs():
-    return RedirectResponse("/docs", status_code=status.HTTP_302_FOUND)
 
 
 @app.get("/wasserstand/sachsen/{place}")
@@ -54,33 +44,4 @@ def not_found_handler(request: Request, exc: HTTPException):
 
 
 if __name__ == "__main__":
-    print("Getting data...")
-    places = ["bautzen", "schirgiswalde", "dresden"]
-    bautzen = wasserpegel.get_wasserpegel(places[0])
-    schirgiswalde = wasserpegel.get_wasserpegel(places[1])
-    dresden = wasserpegel.get_wasserpegel(places[2])
-    print(bautzen)
-    print(schirgiswalde)
-    print(dresden)
-    print("-" * 40)
-    booleans = [False, False, False]
-    while True:
-        bautzen_updated = wasserpegel.get_wasserpegel(places[0])
-        if bautzen != bautzen_updated:
-            bautzen = bautzen_updated
-            print(bautzen)
-            booleans[0] = True
-        schirgiswalde_updated = wasserpegel.get_wasserpegel(places[1])
-        if schirgiswalde != schirgiswalde_updated:
-            schirgiswalde = schirgiswalde_updated
-            print(schirgiswalde)
-            booleans[1] = True
-        dresden_updated = wasserpegel.get_wasserpegel(places[2])
-        if dresden != dresden_updated:
-            dresden = dresden_updated
-            print(dresden)
-            booleans[2] = True
-        if booleans[0] and booleans[1] and booleans[2]:
-            print("-" * 40)
-            booleans = [False, False, False]
-        sleep(60)
+    cli_tool()
